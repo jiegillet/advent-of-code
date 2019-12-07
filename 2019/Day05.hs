679ee15133325c6a615c271dba685f2b8bff9be2
Jérémie Gillet <jie.gillet@gmail.com>
2019-12-07 14:27:44 +0900

    Day 5 refactor

--- 2019/Day05.hs
+++ 2019/Day05.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day05 where
 
 import Data.Array
@@ -7,57 +9,57 @@ main :: IO ()
 main = do
   ints <- read . (\i ->  "[" ++ i ++ "]") <$> readFile "Day05.txt"
   let prog = listArray (0, length ints - 1) ints
-      intCode = IntCode 0 prog Position []
+      intCode = IntCode 0 prog []
   print $ part1 [1] intCode
   print $ part2 [5] intCode
-
-data OpCode
-  = Add
-  | Mult
-  | Input
-  | Output
-  | JumpT
-  | JumpF
-  | LessThan
-  | Eq
-  | End deriving Show
-
-data IntCode = IntCode
-  { pos :: Int
-  , prog :: Array Int Int
-  , mode :: Mode
-  , output :: [Int]
-  } deriving Show
-
-data Mode = Position | Immediate deriving Show
-
-updateProg :: (Array Int Int -> Array Int Int) -> IntCode -> IntCode
-updateProg f i = i { prog = f (prog i) }
-
-updatePos :: (Int -> Int) -> IntCode -> IntCode
-updatePos f i = i { pos = f (pos i) }
-
-updateOutput :: ([Int] -> [Int]) -> IntCode -> IntCode
-updateOutput f i = i { output = f (output i) }
-
 
 part1 :: [Int] -> IntCode -> [Int]
 part1 inputs = output . execState (runProgram inputs)
 
+part2 :: [Int] -> IntCode -> [Int]
 part2 = part1
 
-getMode :: Int -> (OpCode, [Mode])
-getMode 99 = (End, [])
-getMode n = case split n of
-    (1, m1, m2) -> (Add, [m1, m2])
-    (2, m1, m2) -> (Mult, [m1, m2])
-    (3, _, _)   -> (Input, [])
-    (4, m1, _)  -> (Output, [m1])
-    (5, m1, m2) -> (JumpT, [m1, m2])
-    (6, m1, m2) -> (JumpF, [m1, m2])
-    (7, m1, m2) -> (LessThan, [m1, m2])
-    (8, m1, m2) -> (Eq, [m1, m2])
-    _ -> error $ "bad code" ++ show n
+data Mode = Position | Immediate deriving Show
+
+data OpCode
+  = Add Mode Mode
+  | Mult Mode Mode
+  | Input
+  | Output Mode
+  | JumpT Mode Mode
+  | JumpF Mode Mode
+  | LessThan Mode Mode
+  | Eq Mode Mode
+  | End
+  deriving Show
+
+data IntCode = IntCode
+  { pos :: Int
+  , prog :: Array Int Int
+  , output :: [Int]
+  } deriving Show
+
+update
+  :: (Int -> Int)
+     -> (Array Int Int -> Array Int Int)
+     -> ([Int] -> [Int])
+     -> IntCode
+     -> IntCode
+update fp fg fo i@(IntCode {..}) =
+  i { pos = fp pos, prog = fg prog, output = fo output  }
+
+getOpCode :: Int -> OpCode
+getOpCode i = case split i of
+    (1, m1, m2) -> Add m1 m2
+    (2, m1, m2) -> Mult m1 m2
+    (3, _, _)   -> Input
+    (4, m1, _)  -> Output m1
+    (5, m1, m2) -> JumpT m1 m2
+    (6, m1, m2) -> JumpF m1 m2
+    (7, m1, m2) -> LessThan m1 m2
+    (8, m1, m2) -> Eq m1 m2
+    (99, _, _)  -> End
+    _ -> error $ "Unknown opcode " ++ show i
   where
     split n = ( mod n 100
               , toMode $ mod (div n 100) 10
@@ -65,75 +67,69 @@ getMode n = case split n of
               )
     toMode 0 = Position
     toMode 1 = Immediate
-    toMode n = error "wrong parameter code " ++ show n
+    toMode n = error ("Unknown parameter code " ++ show n)
 
 getValue :: Mode -> Array Int Int -> Int -> Int
-getValue Position prog i = prog!i
-getValue Immediate _ i = i
+getValue Position prog = (prog!) . (prog!)
+getValue Immediate prog = (prog!)
 
 runProgram :: [Int] -> State IntCode ()
 runProgram inputs = do
-  IntCode i prog mode out <- get
-  case getMode (prog!i) of
-    (End, []) -> return ()
+  IntCode i prog out <- get
 
-    (Add, [m1, m2]) -> do
+  case getOpCode (prog!i) of
+    Add m1 m2 -> do
       let i1 = getValue m1 prog (i+1)
           i2 = getValue m2 prog (i+2)
-      modify $ updateProg (// [(prog!(i+3), prog!i1 + prog!i2)])
-      modify $ updatePos (+4)
+          i3 = getValue Immediate prog (i+3)
+      modify $ update (+4) (// [(i3, i1 + i2)]) id
       runProgram inputs
 
-    (Mult, [m1, m2]) -> do
+    Mult m1 m2 -> do
       let i1 = getValue m1 prog (i+1)
           i2 = getValue m2 prog (i+2)
-      modify $ updateProg (// [(prog!(i+3), prog!i1 * prog!i2)])
-      modify $ updatePos (+4)
+          i3 = getValue Immediate prog (i+3)
+      modify $ update (+4) (// [(i3, i1 * i2)]) id
       runProgram inputs
 
-    (Input, _) -> do
-      modify $ updateProg (// [(prog!(i+1), head inputs)])
-      modify $ updatePos (+2)
+    Input -> do
+      let i1 = getValue Immediate prog (i+1)
+      modify $ update (+2) (// [(i1, head inputs)]) id
       runProgram (tail inputs)
 
-    (Output, [m1]) -> do
+    Output m1 -> do
       let i1 = getValue m1 prog (i+1)
-      modify $ updateOutput (prog!i1:)
-      modify $ updatePos (+2)
+      modify $ update (+2) id (i1:)
       runProgram inputs
 
-    (JumpT, [m1, m2]) -> do
-      let i1 = getValue m1 prog (i+1)
-          i2 = getValue m2 prog (i+2)
-      if prog!i1 /= 0
-        then modify $ updatePos (const (prog!i2))
-        else modify $ updatePos (+3)
-      runProgram inputs
-
-    (JumpF, [m1, m2]) -> do
+    JumpT m1 m2 -> do
       let i1 = getValue m1 prog (i+1)
           i2 = getValue m2 prog (i+2)
-      if prog!i1 == 0
-        then modify $ updatePos (const (prog!i2))
-        else modify $ updatePos (+3)
+          move = if i1 /= 0 then const i2 else (+3)
+      modify $ update move id id
       runProgram inputs
 
-    (LessThan, [m1, m2]) -> do
+    JumpF m1 m2 -> do
       let i1 = getValue m1 prog (i+1)
           i2 = getValue m2 prog (i+2)
-      if prog!i1 < prog!i2
-        then modify $ updateProg (// [(prog!(i+3), 1)])
-        else modify $ updateProg (// [(prog!(i+3), 0)])
-      modify $ updatePos (+4)
+          move = if i1 == 0 then const i2 else (+3)
+      modify $ update move id id
       runProgram inputs
 
-    (Eq, [m1, m2]) -> do
+    LessThan m1 m2 -> do
       let i1 = getValue m1 prog (i+1)
           i2 = getValue m2 prog (i+2)
-      if prog!i1 == prog!i2
-        then modify $ updateProg (// [(prog!(i+3), 1)])
-        else modify $ updateProg (// [(prog!(i+3), 0)])
-      modify $ updatePos (+4)
+          i3 = getValue Immediate prog (i+3)
+          val = if i1 < i2 then 1 else 0
+      modify $ update (+4) (// [(i3, val)]) id
       runProgram inputs
 
-    _ -> error( "Unknown opcode" ++ show (prog!i))
+    Eq m1 m2 -> do
+      let i1 = getValue m1 prog (i+1)
+          i2 = getValue m2 prog (i+2)
+          i3 = getValue Immediate prog (i+3)
+          val = if i1 == i2 then 1 else 0
+      modify $ update (+4) (// [(i3, val)]) id
+      runProgram inputs
+
+    End -> return ()
