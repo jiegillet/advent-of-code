93643d496a70c959b31cf987c8d1724bb076033f
Jérémie Gillet <jie.gillet@gmail.com>
2019-12-09 22:40:42 +0900

    Changed Array to IntMap for performance

--- 2019/Day05.hs
+++ 2019/Day05.hs
@@ -9,8 +9,8 @@ main = do
   print $ part1 intCode [1]
   print $ part2 intCode [5]
 
-part1 :: IntCode -> [Int] -> [Int]
+part1 :: IntCode -> [Integer] -> [Integer]
 part1 = getOutput
 
-part2 :: IntCode -> [Int] -> [Int]
+part2 :: IntCode -> [Integer] -> [Integer]
 part2 = getOutput
--- 2019/IntCode.hs
+++ 2019/IntCode.hs
@@ -2,13 +2,14 @@
 
 module IntCode (IntCode, getOutput, mkIntCode) where
 
-import           Control.Monad.State.Strict
-import           Data.Array
+import Control.Monad.State.Strict
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
 
 mkIntCode :: [Integer] -> IntCode
 mkIntCode ints =  IntCode 0 prog [] 0
   where
-    prog = listArray (0, 2^11-1) (ints ++ repeat 0)
+    prog = M.fromList $ zip [0..] ints
 
 getOutput :: IntCode -> [Integer] -> [Integer]
 getOutput code inputs = reverse $ output $ execState (runProgram inputs) code
@@ -34,14 +35,14 @@ data OpCode
 
 data IntCode = IntCode
   { pos    :: Integer
-  , prog   :: Array Integer Integer
+  , prog   :: IntMap Integer
   , output :: [Integer]
   , base   :: Integer
   } deriving Show
 
 update
   :: (Integer -> Integer)
-     -> (Array Integer Integer -> Array Integer Integer)
+     -> (IntMap Integer -> IntMap Integer)
      -> ([Integer] -> [Integer])
      -> (Integer -> Integer)
      -> IntCode
@@ -53,14 +54,14 @@ getOpCode :: Integer -> OpCode
 getOpCode i = case split i of
     (1, m1, m2, m3) -> Add m1 m2 m3
     (2, m1, m2, m3) -> Mult m1 m2 m3
-    (3, m1, _,  _)  -> Input m1
-    (4, m1, _,  _)  -> Output m1
-    (5, m1, m2, _)  -> JumpT m1 m2
-    (6, m1, m2, _)  -> JumpF m1 m2
+    (3, m1, _,  _ ) -> Input m1
+    (4, m1, _,  _ ) -> Output m1
+    (5, m1, m2, _ ) -> JumpT m1 m2
+    (6, m1, m2, _ ) -> JumpF m1 m2
     (7, m1, m2, m3) -> LessThan m1 m2 m3
     (8, m1, m2, m3) -> Eq m1 m2 m3
-    (9, m1, _,  _)  -> AdjustBase m1
-    (99, _, _,  _)  -> End
+    (9, m1, _,  _ ) -> AdjustBase m1
+    (99, _, _,  _ ) -> End
     _               -> error $ "Unknown opcode " ++ show i
   where
     split n = ( mod n 100
@@ -71,80 +72,85 @@ getOpCode i = case split i of
     toMode 0 = Position
     toMode 1 = Immediate
     toMode 2 = Relative
-    toMode n = error ("Unknown parameter code " ++ show n)
+    toMode n = error $ "Unknown parameter code " ++ show n
 
 runProgram :: [Integer] -> State IntCode ()
 runProgram inputs = do
   intcode@(IntCode i prog _ base) <- get
 
-  let getValue :: Mode -> Integer -> Integer
-      getValue Position  = (prog!) . (prog!)
-      getValue Immediate = (prog!)
-      getValue Relative  = (prog!) . (+base) . (prog!)
+  let getInt :: Integer -> Integer
+      getInt i = M.findWithDefault 0 (fromInteger i) prog
+
+      replace :: Integer -> Integer -> IntMap Integer -> IntMap Integer
+      replace i j = M.insert (fromInteger i) j
+
+      readValue :: Mode -> Integer -> Integer
+      readValue Position  = getInt . getInt
+      readValue Immediate = getInt
+      readValue Relative  = getInt . (+base) . getInt
 
       writeValue :: Mode -> Integer -> Integer
-      writeValue Position  = (prog!)
-      writeValue Immediate = error "Writing instruction in immediate mode "
-      writeValue Relative  = (+base) . (prog!)
+      writeValue Position  = getInt
+      writeValue Immediate = error "Writing instruction in immediate mode"
+      writeValue Relative  = (+base) . getInt
 
-
-  case getOpCode (prog!i) of
+  case getOpCode (prog M.! (fromInteger i)) of
     Add m1 m2 m3 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           i3 = writeValue m3 (i+3)
-      modify $ update (+4) (// [(i3, i1 + i2)]) id id
+      modify $ update (+4) (replace i3 (i1 + i2)) id id
       runProgram inputs
 
     Mult m1 m2 m3 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           i3 = writeValue m3 (i+3)
-      modify $ update (+4) (// [(i3, i1 * i2)]) id id
+      modify $ update (+4) (replace i3 (i1 * i2)) id id
       runProgram inputs
 
     Input m1 -> do
       let i1 = writeValue m1 (i+1)
-      modify $ update (+2) (// [(i1, head inputs)]) id id
+      modify $ update (+2) (replace i1 (head inputs)) id id
       runProgram (tail inputs)
 
     Output m1 -> do
-      let i1 = getValue m1 (i+1)
+      let i1 = readValue m1 (i+1)
       modify $ update (+2) id (i1:) id
       runProgram inputs
 
     JumpT m1 m2 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           move = if i1 /= 0 then const i2 else (+3)
       modify $ update move id id id
       runProgram inputs
 
     JumpF m1 m2 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           move = if i1 == 0 then const i2 else (+3)
       modify $ update move id id id
       runProgram inputs
 
     LessThan m1 m2 m3 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           i3 = writeValue m3 (i+3)
           val = if i1 < i2 then 1 else 0
-      modify $ update (+4) (// [(i3, val)]) id id
+      modify $ update (+4) (replace i3 val) id id
       runProgram inputs
 
     Eq m1 m2 m3 -> do
-      let i1 = getValue m1 (i+1)
-          i2 = getValue m2 (i+2)
+      let i1 = readValue m1 (i+1)
+          i2 = readValue m2 (i+2)
           i3 = writeValue m3 (i+3)
           val = if i1 == i2 then 1 else 0
-      modify $ update (+4) (// [(i3, val)]) id id
+      modify $ update (+4) (replace i3 val) id id
       runProgram inputs
 
     AdjustBase m1 -> do
-      let i1 = getValue m1 (i+1)
+      let i1 = readValue m1 (i+1)
       modify $ update (+2) id id (+i1)
       runProgram inputs
 
